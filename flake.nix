0e5435fc93019b92cc39b4e15d722a6be4d8a348
ash <ashkan.rahnavardi@gmail.com>
2024-06-02 22:45:21 -0700

    moving hyprland conf to seperate file

--- flake.nix
+++ flake.nix
@@ -43,47 +43,4 @@
 
 }
 
-# {
-#
-#
-#   description = "My first flake!";
-#
-#
-#   inputs = {
-#     nixpkgs.url = "nixpkgs/nixos-24.05";
-#     home-manager.url = "github:nix-community/home-manager/release-24.05";
-#     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-#     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-#
-#     hyprland-plugins = {
-#       url = "github:hyprwm/hyprland-plugins";
-#       inputs.hyprland.follows = "hyprland";
-#     };
-#
-#   };
-#
-#
-#   outputs = { self, nixpkgs, home-manager, ...} @ inputs:
-#     let
-#       lib = nixpkgs.lib;
-#       system = "x86_64-linux";
-#       pkgs = nixpkgs.legacyPackages.${system};
-#     in {
-#     nixosConfigurations = {
-#       nixos = lib.nixosSystem {
-#         inherit system;
-#         specialArgs = { inherit inputs; }; # this is the important part
-#         modules = [ ./configuration.nix ];
-#       };
-#     };
-#     homeConfigurations = {
-#       ash = home-manager.lib.homeManagerConfiguration {
-#         inherit pkgs;
-#         modules = [
-#           ./home.nix
-#         ];
-#       };
-#     };
-#   };
-#
-# }
+
