578e2c09243ee17fa90fcb8809b2bf5519e9614b
ash <ashkan.rahnavardi@gmail.com>
2024-06-03 18:34:57 -0700

    added 3 monitor settings

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -24,7 +24,8 @@ in {
       splash = false
       preload = ${wallpaper}
       wallpaper = DP-1, ${wallpaper}
-      wallpaper = eDP-1, ${wallpaper}
+      wallpaper = DP-2, ${wallpaper}
+      wallpaper = DP-3, ${wallpaper}
     '';
 
     "hypr/hypridle.conf".text = ''
@@ -37,7 +38,27 @@ in {
 
     "hypr/hyprlock.conf".text = ''
       background {
-          monitor =
+          monitor = DP-1
+          path = ${lock}
+          blur_passes = 3
+          contrast = 0.8916
+          brightness = 0.8172
+          vibrancy = 0.1696
+          vibrancy_darkness = 0.0
+      }
+
+          background {
+          monitor = DP-2
+          path = ${lock}
+          blur_passes = 3
+          contrast = 0.8916
+          brightness = 0.8172
+          vibrancy = 0.1696
+          vibrancy_darkness = 0.0
+      }
+
+          background {
+          monitor = DP-3
           path = ${lock}
           blur_passes = 3
           contrast = 0.8916
@@ -109,10 +130,10 @@ in {
         valign = center
       }
 
-      # eDP-1 Conifg
+      # DP-2 Conifg
       input-field {
-          monitor = eDP-1
-          size = 500, 120
+          monitor = DP-2
+          size = 250, 60
           outline_thickness = 2
           dots_size = 0.2 # Scale of input-field height, 0.2 - 0.8
           dots_spacing = 0.2 # Scale of dots' absolute size, 0.0 - 1.0
@@ -132,10 +153,10 @@ in {
 
       # Date
       label {
-        monitor = eDP-1
+        monitor = DP-2
         text = cmd[update:1000] echo "<span>$(date '+%A, %d %B')</span>"
         color = rgba(255, 255, 255, 0.8)
-        font_size = 30
+        font_size = 15
         font_family = JetBrains Mono Nerd Font Mono ExtraBold
         position = 0, -400
         halign = center
@@ -144,10 +165,10 @@ in {
 
       # Time
       label {
-          monitor = eDP-1
+          monitor = DP-2
           text = cmd[update:1000] echo "<span>$(date '+%H:%M')</span>"
           color = rgba(255, 255, 255, 0.8)
-          font_size = 240
+          font_size = 120
           font_family = JetBrains Mono Nerd Font Mono ExtraBold
           position = 0, -400
           halign = center
@@ -156,14 +177,74 @@ in {
 
       # Keyboard layout
       label {
-        monitor = eDP-1
+        monitor = DP-2
         text = $LAYOUT
         color = rgba(255, 255, 255, 0.9)
-        font_size = 20
+        font_size = 10
         font_family = JetBrains Mono Nerd Font Mono
-        position = 0, -230
+        position = 0, -175
         halign = center
         valign = center
+      }
+
+
+      # DP-3 Conifg
+      input-field {
+          monitor = DP-3
+          size = 250, 60
+          outline_thickness = 2
+          dots_size = 0.2 # Scale of input-field height, 0.2 - 0.8
+          dots_spacing = 0.2 # Scale of dots' absolute size, 0.0 - 1.0
+          dots_center = true
+          outer_color = rgba(0, 0, 0, 0)
+          inner_color = rgba(0, 0, 0, 0.5)
+          font_color = rgb(200, 200, 200)
+          fade_on_empty = false
+          capslock_color = -1
+          placeholder_text = <i><span foreground="##e6e9ef">Password</span></i>
+          fail_text = <i>$FAIL <b>($ATTEMPTS)</b></i>
+          hide_input = false
+          position = 0, -120
+          halign = center
+          valign = center
+      }
+
+      # Date
+      label {
+        monitor = DP-3
+        text = cmd[update:1000] echo "<span>$(date '+%A, %d %B')</span>"
+        color = rgba(255, 255, 255, 0.8)
+        font_size = 15
+        font_family = JetBrains Mono Nerd Font Mono ExtraBold
+        position = 0, -400
+        halign = center
+        valign = top
+      }
+
+      # Time
+      label {
+          monitor = DP-3
+          text = cmd[update:1000] echo "<span>$(date '+%H:%M')</span>"
+          color = rgba(255, 255, 255, 0.8)
+          font_size = 120
+          font_family = JetBrains Mono Nerd Font Mono ExtraBold
+          position = 0, -400
+          halign = center
+          valign = top
+      }
+
+      # Keyboard layout
+      label {
+        monitor = DP-3
+        text = $LAYOUT
+        color = rgba(255, 255, 255, 0.9)
+        font_size = 10
+        font_family = JetBrains Mono Nerd Font Mono
+        position = 0, -175
+        halign = center
+        valign = center
+      }
+
       }
     '';
   };
