d100d22900780bebad1d9ed81bf7a7f00a303756
ash <ashkan.rahnavardi@gmail.com>
2024-06-05 16:13:45 -0700

    add lua config

--- files/configs/nvim/lua/config/options.lua
+++ files/configs/nvim/lua/config/options.lua
@@ -3,3 +3,30 @@
 -- Add any additional options here
 vim.opt.winbar = "%=%m %f"
 vim.g.lazygit_config = false
+
+-- Create an augroup for the conf file type
+vim.cmd([[
+  augroup conf_syntax
+    autocmd!
+    autocmd BufRead,BufNewFile *.conf set filetype=conf
+  augroup END
+]])
+
+-- Optional: If you want to add custom syntax highlighting rules for conf files
+-- You can define them here if not already provided by default
+
+-- Example: Basic syntax highlighting for conf files
+vim.cmd([[
+  if exists("b:current_syntax")
+    finish
+  endif
+  syntax match confComment "#.*$"
+  syntax match confSection "^\[.*\]"
+  syntax match confKey "^\s*[a-zA-Z0-9_\-]\+\s*="
+  syntax match confValue "=\s*\zs.*"
+  highlight link confComment Comment
+  highlight link confSection Identifier
+  highlight link confKey Keyword
+  highlight link confValue String
+  let b:current_syntax = "conf"
+]])
