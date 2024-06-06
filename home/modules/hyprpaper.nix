cba9ccca0e6bdfec9c7ecacd1fe86b415d539139
ash <ashkan.rahnavardi@gmail.com>
2024-06-05 22:17:23 -0700

    fixed file type

--- home/modules/hyprpaper.nix
+++ home/modules/hyprpaper.nix
@@ -1,8 +1,7 @@
 {...}: let
   wallpaper = builtins.toPath "/home/ash/Pictures/walls/rice/neon_car.png";
   wallpaper2 = builtins.toPath "/home/ash/Pictures/walls/rice/purp_jap.png";
-  #  wallpaper3 = builtins.toPath "/home/ash/Pictures/walls/rice/road-moon_vert.png";
-  wallpaper3 = builtins.toPath "/home/ash/Pictures/walls/RetroWave/city.png";
+  wallpaper3 = builtins.toPath "/home/ash/Pictures/walls/rice/rice_vert.png";
 in {
   services.hyprpaper = {
     enable = true;
