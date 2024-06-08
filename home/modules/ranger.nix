c99b36eb4f78b8994d048bf11876ddca6444c451
ash <ashkan.rahnavardi@gmail.com>
2024-06-08 10:00:24 -0700

    add correct pistol path & dependencies

--- home/modules/ranger.nix
+++ home/modules/ranger.nix
@@ -1,7 +1,10 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    ueberzugpp
+    ueberzug
+    imagemagick
     w3m
+    pistol
+    python312Packages.typecode-libmagic
   ];
 
   programs.ranger = {
@@ -9,7 +12,9 @@
 
     extraConfig = ''
       set preview_images true
+      set preview_images_method ueberzug
       set show_hidden true
+      set preview_script /home/ash/.nix-profile/bin/pistol
     '';
   };
 }
