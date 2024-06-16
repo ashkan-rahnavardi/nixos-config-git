f5465cfef737e23704e61673adee6bff6771ec17
ash <ashkan.rahnavardi@gmail.com>
2024-06-15 17:45:49 -0700

    back to normal neovim setup

--- home/home.nix
+++ home/home.nix
@@ -10,8 +10,8 @@
     ./modules/gpg.nix
     ./modules/gtk.nix
     ./modules/lazygit.nix
-    ./modules/neovim/default.nix
-    # ./modules/neovim.nix
+    # ./modules/neovim/default.nix
+    ./modules/neovim.nix
     ./modules/scripts.nix
     ./modules/spicetify.nix
     ./modules/tmux.nix
