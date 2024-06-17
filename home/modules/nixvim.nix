855721f186fb051d9d710cfefce2f548b9691751
ash <ashkan.rahnavardi@gmail.com>
2024-06-17 01:28:57 -0700

    ashvim up & running well

--- home/modules/nixvim.nix
+++ home/modules/nixvim.nix
@@ -6,7 +6,7 @@
   nixvim' = inputs.nixvim.packages."x86_64-linux".default;
   nvim = nixvim'.nixvimExtend {
     config = {
-      theme = lib.mkForce "paradise";
+      theme = lib.mkForce "tokyonight";
     };
   };
 in {
